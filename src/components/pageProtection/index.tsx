2146013113db64c094a8c24076f27c2cf8da0f26
ziyafenn <ziya.fenn@gmail.com>
2023-08-10 13:40:28 +0200

    password protection css

--- src/components/pageProtection/index.tsx
+++ src/components/pageProtection/index.tsx
@@ -1,14 +1,16 @@
 "use client";
+
 import { useState } from "react";
 import styles from "./pageProtection.module.css";
 
 export default function PageProtection() {
   const [password, setPassword] = useState<string>("");
   const [passwordIncorrect, setPasswordIncorrect] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const onSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-
+    setLoading(true);
     const request = await fetch(`/api/case-study`, {
       body: JSON.stringify({
         password,
@@ -19,27 +21,57 @@ export default function PageProtection() {
       method: "post",
     });
 
-    if (request.status !== 200) return setPasswordIncorrect(true);
-    window.location.reload();
+    if (request.status !== 200)
+      return setPasswordIncorrect(true), setLoading(false);
+    else window.location.reload();
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (passwordIncorrect) setPasswordIncorrect(false);
+    setPassword(e.target.value);
   };
 
   return (
     <div className={styles.backdrop}>
-      <div>
-        <h3>Enter password</h3>
-        <form onSubmit={(e) => onSubmit(e)}>
-          <div>
-            <input
-              type="password"
-              name="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-
-            <button type="submit">Login</button>
-          </div>
-          {passwordIncorrect && "Password Incorrect"}
-        </form>
+      <div className={styles.container}>
+        <button
+          type="button"
+          className={styles.closeButton}
+          onClick={() => window.location.assign("/")}
+        >
+          X
+        </button>
+        <div className={styles.content}>
+          <h3>Password required</h3>
+          <p>
+            Due to NDA, i'm not able to publicly display my work. If you want to
+            read my case studies, please enter the provided password.
+          </p>
+          <form onSubmit={(e) => onSubmit(e)}>
+            <label>Password</label>
+            <div className={styles.fieldset}>
+              <input
+                type="password"
+                name="password"
+                value={password}
+                onChange={onChange}
+              />
+              <button
+                type="submit"
+                disabled={loading}
+                className={styles.submitButton}
+              >
+                {loading ? "..." : "Submit"}
+              </button>
+            </div>
+            <div
+              className={styles.passwordIncorrect}
+              style={{ visibility: passwordIncorrect ? "visible" : "hidden" }}
+            >
+              Password Incorrect
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
--- src/helpers/fetchEntries.ts
+++ src/helpers/fetchEntries.ts
@@ -1,8 +1,8 @@
 import { Entries } from "./types";
 
 export const fetchEntries = async (props: { fetchAll: boolean }) => {
-  const allEntries = `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}`;
-  const entriesPreview = `${allEntries}&content_type=caseStudy&select=fields.title,fields.description,fields.tags,fields.color,fields.id,fields.slug`;
+  const allEntries = `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=caseStudy`;
+  const entriesPreview = `${allEntries}&select=fields.title,fields.description,fields.tags,fields.color,fields.id,fields.slug`;
   const response = await fetch(props.fetchAll ? allEntries : entriesPreview);
   const data: Entries = await response.json();
   return data;
